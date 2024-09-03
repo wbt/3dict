df9e4527f451050cfb78fa5e8c222f8c7cb64e62
WBT <WBT@users.noreply.github.com>
2024-09-03 15:38:01 -0400

    Add purpose of contract

--- packages/hardhat/contracts/USDME.sol
+++ packages/hardhat/contracts/USDME.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Generated with https://wizard.openzeppelin.com/
+// This contract is intended to simulate an existing widely used ERC20 already deployed on a main chain.
+// It is a token only intended to be used for development & testing on chains that don't have a predeployed token ecosystem.
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
