189077e4ed9bfb9c085eeb7a666c6d68049ab7f0
WBT <WBT@users.noreply.github.com>
2024-09-03 15:36:37 -0400

    Copy in an ERC-20 from OpenZeppelin wizard

---
+++ packages/hardhat/contracts/USDME.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDME is ERC20, ERC20Permit, Ownable {
+    constructor(address initialOwner)
+        ERC20("USDME", "USDME")
+        ERC20Permit("USDME")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+}
