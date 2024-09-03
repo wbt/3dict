8570d80bc08d62b166fe1719593875a05ef0c6d9
WBT <WBT@users.noreply.github.com>
2024-09-03 16:31:38 -0400

    Use OpenZeppelin Ownable

--- packages/hardhat/contracts/App3Dict.sol
+++ packages/hardhat/contracts/App3Dict.sol
@@ -4,17 +4,15 @@ pragma solidity >=0.8.0 <0.9.0;
 // Useful for debugging. Remove when deploying to a live network.
 import "hardhat/console.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * A smart contract that allows changing a state variable of the contract and tracking the changes
  * It also allows the owner to withdraw the Ether in the contract
  * @author BuidlGuidl
  */
-contract App3Dict {
+contract App3Dict is Ownable{
 	// State Variables
-	address public immutable owner;
 	string public greeting = "Building Unstoppable Apps!!!";
 	bool public premium = false;
 	uint256 public totalCounter = 0;
@@ -30,16 +28,8 @@ contract App3Dict {
 
 	// Constructor: Called once on contract deployment
 	// Check packages/hardhat/deploy/00_deploy_your_contract.ts
-	constructor(address _owner) {
-		owner = _owner;
-	}
-
-	// Modifier: used to define a set of rules that must be met before or after a function is executed
-	// Check the withdraw() function
-	modifier isOwner() {
-		// msg.sender: predefined variable that represents address of the account that called the current function
-		require(msg.sender == owner, "Not the Owner");
-		_;
+	constructor(address initialOwner) {
+		Ownable(initialOwner)
 	}
 
 	/**
@@ -73,9 +63,9 @@ contract App3Dict {
 
 	/**
 	 * Function that allows the owner to withdraw all the Ether in the contract
-	 * The function can only be called by the owner of the contract as defined by the isOwner modifier
+	 * The function can only be called by the owner of the contract as defined by the modifier
 	 */
-	function withdraw() public isOwner {
+	function withdraw() public onlyOwner {
 		(bool success, ) = owner.call{ value: address(this).balance }("");
 		require(success, "Failed to send Ether");
 	}
