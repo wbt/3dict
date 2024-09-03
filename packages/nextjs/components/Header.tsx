23ada869e639e8b54b44401665317d692fae3279
WBT <WBT@users.noreply.github.com>
2024-09-03 16:13:30 -0400

    Update app name in header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -93,8 +93,8 @@ export const Header = () => {
             <Image alt="3Dict logo" className="cursor-pointer" fill src="/crystal-ball-1--streamline-emoji.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">3Dict</span>
+            <span className="text-xs">Own your predictions</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
