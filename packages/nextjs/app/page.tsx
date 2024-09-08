503b4bf0b267de885de10e97bb47288757153470
WBT <WBT@users.noreply.github.com>
2024-09-08 11:44:07 -0400

    Rename debug to on-chain fns

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
@@ -33,11 +33,10 @@ const Home: NextPage = () => {
       <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
         <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
           <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-            <BugAntIcon className="h-8 w-8 fill-secondary" />
             <p>
               Tinker with your smart contract using the{" "}
               <Link href="/debug" passHref className="link">
-                Debug Contracts
+                On-Chain Functions
               </Link>{" "}
               tab.
             </p>
